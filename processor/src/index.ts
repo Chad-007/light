08517bd33300615b8994abbae30b4841da6cc923
alan <alansebastian484@gmail.com>
2025-07-25 15:36:10 +0530

    updated processor

--- processor/src/index.ts
+++ processor/src/index.ts
@@ -1,16 +1,29 @@
 import { PrismaClient } from "./generated/prisma";
+import { Kafka } from "kafkajs";
 const client = new PrismaClient();
 
+const kafka = new Kafka({
+  clientId: 'outbox-processor',
+  brokers: ['localhost:9092']
+})
+
 async function hey(){
+
+    const producer = kafka.producer();
+    await producer.connect();
     while(1){
          const pending = await client.outBox.findMany({
             where: {},
             take: 10,
          })
-         pending.forEach(row => {
-         })
-
-
-
+         producer.send({    
+                topic: 'outbox_events',
+                messages: pending.map(row => ({ value: row.zaprunid }))  
+            }).then(() => {
+                console.log("send event");
+            }).catch(err => {
+                console.error(`Failed to send outbox event`, err);
+            });
 }
 }
+hey()
